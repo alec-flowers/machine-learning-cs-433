c14011b0c990ce8a96ec58f97d475739f94775fc
aglavac <a.glavackij@gmx.de>
2020-10-01 10:45:15 +0200

    refact: introduced comute_mse in gradient descent algorithms.

--- gradient descent.py
+++ gradient descent.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 """Gradient Descent"""
 
-from costs import *
+import numpy as np
+from costs import compute_mse, compute_error
+
 
 def compute_gradient(y, tx, w):
-    """Compute the gradient."""
-    N = len(y)
-    e = compute_gradient(y, tx, w)   
-    gradient = -1/N * (np.transpose(tx)).dot(e))
-    return e, gradient
+	"""Compute the gradient."""
+	e = compute_error(y, tx, w)
+	N = len(y)
+	gradient = -1 / N * (np.transpose(tx)).dot(e)
+	return e, gradient
 
 
 def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """Gradient descent algorithm."""
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        """Compute gradient and loss"""
-        e, gradient = compute_gradient(y, tx, w)
-        loss = compute_mse(y, tx, w)
-        w = w - gamma*gradient 
-        
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
-    return losses, ws
+	"""Gradient descent algorithm."""
+	# Define parameters to store w and loss
+	ws = [initial_w]
+	losses = []
+	w = initial_w
+	for n_iter in range(max_iters):
+		"""Compute gradient and loss"""
+		e, gradient = compute_gradient(y, tx, w)
+		w = w - gamma * gradient
+		loss = compute_mse(y, tx, w)
+		# store w and loss
+		ws.append(w)
+		losses.append(loss)
+		print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+			bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+	return losses, ws
