8a180d27203a03092df3fee7ffc91932c6a467e4
Alec Flowers <alec.flowers00@gmail.com>
2020-10-06 23:17:31 +0200

    FEAT: Start implementation file

--- costs.py
+++ costs.py
@@ -2,13 +2,11 @@
 """Functions used to compute the loss."""
 import numpy as np
 
-
 def compute_error(y, tx, w):
 	"""
 	Computes error e.
 	"""
 	return y - tx.dot(w)
-
 
 def compute_mse(y, tx, w):
 	"""Calculate the loss using MSE (Mean Squared Error). """
@@ -17,7 +15,6 @@ def compute_mse(y, tx, w):
 	loss = 1 / (2 * N) * np.sum(e ** 2)
 	return loss
 
-
 def compute_mae(y, tx, w):
 	"""Calculate the loss using MAE (Mean Absolute Error)."""
 	N = len(y)
@@ -25,6 +22,53 @@ def compute_mae(y, tx, w):
 	loss = 1 / (2 * N) * np.sum(np.abs(e))
 	return loss
 
+
+def mse(y, y_hat):
+	'Calculates and returns MSE between two vectors of same size'
+    return np.sum((y - y_hat)**2) / (2 * len(y_hat))
+    
+def mae(y, y_hat):
+	'Calculates and returns MAE between two vectors of same size'
+    return np.sum(np.abs(y - y_hat)) / len(y_hat)
+
+def rmse(y, y_hat):
+	'Calculates and returns RMSE between two vectors of same size'
+    return np.sqrt(2*np.sum((y - y_hat)**2) / (2 * len(y_hat)))
+
+def compute_loss_alec(y, tx, w, error_fn = 'MSE'):
+    """
+	Calculate the loss between dependent variable and prediction.
+
+	Parameters
+	----------
+	y : ndarray of shape (n_samples,)
+		Array of labels
+
+	tx : ndarray of shape (n_samples, n_features)
+		Training data
+
+	w : ndarray of shape (n_weights,)
+		Weight vector
+	
+	error_fn : string selecting ['MSE', 'MAE', 'RMSE']
+
+	Returns
+	----------
+	error : np.float64
+		error between dependent variable and prediction
+	
+	"""
+
+    y_hat = np.sum(tx * w,axis=1)
+    if error_fn == 'MSE':
+        error = mse(y, y_hat)
+    elif error_fn == 'MAE':
+        error = mae(y, y_hat)
+	elif error_fn == 'RMSE':
+		error = rmse(y, y_hat)
+    else:
+        print('Did not match a loss function')
+    return error
 
 def test():
 	y = np.array([2, 3, 4, 3])
---
+++ implementation.py
@@ -0,0 +1,59 @@
+import numpy as np
+from costs import compute_loss_alec
+
+def least_squares(y, tx):
+    """Least Squares
+	
+	Parameters
+	----------
+	y : ndarray of shape (n_samples,)
+		Array of labels
+
+	tx : ndarray of shape (n_samples, n_features)
+		Training data
+
+	Returns
+	----------
+    w : np.array of shape(1, D)
+		Optimal weights calculated using normal equations.
+
+	loss : np.float64
+		RMSE loss for corresponding weight value
+		
+	"""
+    w = np.linalg.solve(tx.T.dot(tx),tx.T.dot(y))
+    loss = compute_loss_alec(y, tx, w, 'RMSE')
+    return w, loss
+
+
+def ridge_regression(y, tx, lambda_):
+    """Ridge Regression 
+	
+	Parameters
+	----------
+	y : ndarray of shape (n_samples,)
+		Array of labels
+
+	tx : ndarray of shape (n_samples, n_features)
+		Training data
+
+	lambda : float [0, 1]
+		parsimony penalty
+
+	Returns
+	----------
+    w : np.array of shape(1, D)
+		Optimal weights calculated using normula equations.
+
+	loss : np.float64
+		RMSE loss for corresponding weight value
+		
+	"""
+
+    N = len(tx)
+    D = tx.shape[1]
+    
+    w = np.linalg.solve(tx.T.dot(tx)+2*N*lambda_*np.identity(D),tx.T.dot(y))
+    loss = compute_loss_alec(y, tx, w, 'RMSE')
+    
+    return w, loss
