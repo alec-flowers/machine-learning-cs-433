FIX: Gradient Descent working

implementation.py
@@ -26,7 +26,7 @@ def least_squares(y, tx):
 		
 	"""
 	w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
-	loss = compute_loss_alec(y, tx, w, 'RMSE')
+	loss = compute_loss(y, tx, w, 'RMSE')
 	return w, loss
 
 
@@ -58,7 +58,7 @@ def ridge_regression(y, tx, lambda_):
 	D = tx.shape[1]
 
 	w = np.linalg.solve(tx.T.dot(tx) + 2 * N * lambda_ * np.identity(D), tx.T.dot(y))
-	loss = compute_loss_alec(y, tx, w, 'RMSE')
+	loss = compute_loss(y, tx, w, 'RMSE')
 
 	return w, loss
 
@@ -92,7 +92,7 @@ def compute_gradient(y, tx, w):
 	return gradient
 
 
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
+def gradient_descent(y, tx, initial_w, max_iters, gamma, error_type = 'MSE'):
 	"""Gradient Descent algorithm.
 
 	Every epoch takes sums errors across all y - e and is therefore computationally more expensive than SGD.
@@ -128,18 +128,18 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
 
 	W0 = 0
 	ws = [initial_w]
-	losses = [compute_mse(y, tx, ws[W0])]
+	losses = [compute_loss(y, tx, ws[W0], error_type)]
 
 	for n_iter in range(max_iters):
 		gradient = compute_gradient(y, tx, ws[-1])
 		w = ws[-1] - gamma * gradient
-		loss = compute_mse(y, tx, w)
+		loss = compute_loss(y, tx, w, error_type)
 
 		ws.append(w)
 		losses.append(loss)
 
-		print("GD({bi}/{ti}): loss={l:.6f}, w0={w0:.3f}, w1={w1:.3f}".format(
-			bi=n_iter, ti=max_iters - 1, l=losses[-1], w0=w[0], w1=w[1]))
+		print("GD({bi}/{ti}): loss={l:.6f}".format(
+			bi=n_iter, ti=max_iters - 1, l=losses[-1]))
 
 	return ws[-1], losses[-1]
 

train.py
@@ -6,19 +6,21 @@ from kfold_cv import ParameterGrid, cross_validation, build_k_indices
 def best_model_selection(model, hyperparameters, x, y, k_fold=4, seed=1):
     hyperparam = ParameterGrid(hyperparameters)  ###Alec's function!!! D'aquí fa una combinació dels hiperparàmetres
     loss = []
+    weights = []
     for hp in hyperparam:
         k_indices = build_k_indices(y, k_fold, seed) #!!
         loss_list = []
         for k in range(k_fold):
-            loss_tr, loss_te = cross_validation(y, x, k_indices, k, hp, model)
+            loss_tr, loss_te, weight = cross_validation(y, x, k_indices, k, hp, model)
             loss_list.append(loss_te)
         loss.append(np.mean(loss_list))   #This is a list of loss* for each group of hyperparameters
+        weights.append(weight)
 
     loss_star = min(loss)   #This is the best loss*, which corresponds to a specific set of hyperparameters
     hp_star = list(hyperparam)[loss.index(loss_star)]  #this is the hyperparameter that corresponds to the best loss*
+    w = weights[loss.index(loss_star)]
 
-
-    return(hp_star, loss_star)
+    return(hp_star, loss_star, w)
 
 
         #JO FAIG EL QUE SERIA EL CROSS_VALIDATION_DEMO:
@@ -31,15 +33,17 @@ def best_model_selection(model, hyperparameters, x, y, k_fold=4, seed=1):
         #podria fer això per tots els models i llavors triar el super millor loss*** on triaria quin és el millor model
 
 
-
 if __name__ == "__main__":
     DATA_FOLDER = 'data/'
     TRAIN_DATASET = DATA_FOLDER + "train.csv"
     model = 'gd'
 
-    y, x, ids_train = load_csv_data(TRAIN_DATASET, sub_sample=False)
+    y, x, ids_train = load_csv_data(TRAIN_DATASET, sub_sample = False)
     #print([0 for _ in range(x.shape[1])])
 
-    hyperparameters = {'initial_w':[[0 for _ in range(x.shape[1]+1)]], 'max_iters':[25,10], 'gamma':[.00001]}
-    hp_star, loss_star = best_model_selection(model, hyperparameters, x, y, k_fold=4, seed=1)
-    print('loss*: {}, hp*: {}'.format(loss_star, hp_star))
+    hyperparameters = {'initial_w':[[0 for _ in range(x.shape[1]+1)]],
+                        'max_iters':[500], 
+                        'gamma':[.00000001]}
+
+    hp_star, loss_star, weights = best_model_selection(model, hyperparameters, x, y, k_fold=2, seed=1)
+    print('loss*: {}, hp*: {}, weights: {}'.format(loss_star, hp_star, weights))