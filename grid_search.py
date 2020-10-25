02e1702c2df322d20be1d838e53eaf1e4f675d7a
aglavac <a.glavackij@gmx.de>
2020-10-25 11:50:38 +0100

    FIX: Fixed import errors caused by deleting compute_mse

--- grid_search.py
+++ grid_search.py
@@ -3,27 +3,27 @@
 
 import numpy as np
 
-from costs import compute_mse
+from costs import compute_loss
 
 
 def generate_w(num_intervals):
-	"""Generate a grid of values for w0 and w1."""
-	w0 = np.linspace(-100, 200, num_intervals)
-	w1 = np.linspace(-150, 150, num_intervals)
-	return w0, w1
+    """Generate a grid of values for w0 and w1."""
+    w0 = np.linspace(-100, 200, num_intervals)
+    w1 = np.linspace(-150, 150, num_intervals)
+    return w0, w1
 
 
 def get_best_parameters(w0, w1, losses):
-	"""Get the best w from the result of grid search."""
-	min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
-	return losses[min_row, min_col], w0[min_row], w1[min_col]
+    """Get the best w from the result of grid search."""
+    min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
+    return losses[min_row, min_col], w0[min_row], w1[min_col]
 
 
 def grid_search(y, tx, w0, w1):
-	"""Algorithm for grid search."""
-	losses = np.zeros((len(w0), len(w1)))
-	for i in range(losses.shape[0]):
-		for j in range(losses.shape[1]):
-			w = w0[i], w1[j]
-			losses[i,j] = compute_mse(y, tx, w)   
-	return losses
+    """Algorithm for grid search."""
+    losses = np.zeros((len(w0), len(w1)))
+    for i in range(losses.shape[0]):
+        for j in range(losses.shape[1]):
+            w = w0[i], w1[j]
+            losses[i, j] = compute_loss(y, tx, w, 'MSE')
+    return losses
