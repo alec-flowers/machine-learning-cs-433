b51010f0e58e5752f85ad0ee346978b5fb8ff09f
aglavac <a.glavackij@gmx.de>
2020-10-25 16:14:45 +0100

    FEAT: run.py can now make prediction with the best weights.

--- run.py
+++ run.py
@@ -1,18 +1,12 @@
-from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
+from proj1_helpers import load_csv_data, create_csv_submission
+from costs import predict_labels
 from helpers import read_json
+from kfold_cv import build_folds
+from training import read_best_hyperparameters
+
 from os import path
 import argparse
-
-
-
-def run():
-    model = 'gd'  #CHANGE THIS!!!
-    yb_data_test, input_data_test, ids_test = load_csv_data("Data/test.csv")
-    w = read_weights(model)
-    y_pred = predict_labels(w, input_data_test)
-    #!!!!! WE HAVE TO PUT A 0 WHEN WE SAVE THE WEIGHTS TO THE COLUMNS WE ELIMINATED!!
-
-    create_csv_submission(ids_test, y_pred, "test_submission.csv")
+from timeit import default_timer as timer
 
 
 def read_weights(model):
@@ -25,58 +19,60 @@ def read_weights(model):
     return w
 
 
+def read_test_set():
+    """
+    Reads training data.
 
-# def main(args):
-#     """
-#     Produces the predictions aka the pipeline
-#     """
-#     # get test data, training data and training here only for test purposes
-#     yb_data_train, input_data_train, ids_train = load_csv_data("Data/train.csv")
-#     yb_data_test, input_data_test, ids_test = load_csv_data("Data/test.csv")
-#
-#     # TODO read the config file with weights and hyperparameters
-#     # TODO polynomial expansion
-#
-#     if args.method == "least_squares":
-#         w, rmse = least_squares(yb_data_train, input_data_train)
-#
-#     elif args.method == "linear_regression_gd":
-#         raise NotImplementedError
-#
-#     elif args.method == "linear_regression_sgd":
-#         raise NotImplementedError
-#
-#     elif args.method == "ridge_regression":
-#         raise NotImplementedError
-#
-#     elif args.method == "logistic_regression":
-#         raise NotImplementedError
-#
-#     elif args.method == "reg_logistic_regression":
-#         raise NotImplementedError
-#
-#     else:
-#         raise NotImplementedError
-#
-#     # training error
-#     print(f"RMSE Training: {rmse}")
-#
-#     y_pred = predict_labels(w, input_data_test)
-#
-#     create_csv_submission(ids_test, y_pred, "test_submission.csv")
+    Returns
+    ----------
+    y : ndarray of shape (n_samples,)
+        Array of labels
+
+    x : ndarray of shape (n_samples, n_features)
+        Training data
+
+    ids_test : ndarray of shape (n_samples,)
+        IDs of data
+    """
+
+    DATA_FOLDER = 'Data/'
+    TEST_DATASET = "test.csv"
+
+    start = timer()
+    y, x, ids_test = load_csv_data(path.join(DATA_FOLDER, TEST_DATASET), sub_sample=False)
+    print(f'Data Loaded - Time: {timer() - start:.3f}\n')
+
+    return y, x, ids_test
 
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(description="CLI for the run.py which generates the predictions.")
-#     parser.add_argument('-m', '--method', type=str, help='Which method to use to predict.', required=True)
-#     parser.add_argument('-c', '--config', help="Path to the config file which stores the values for the hyperparameters"
-#                                                "and weights")
-#
-#     args = parser.parse_args()
-#
-#     main(args)
+def main(args):
+    """
+    Produces the predictions aka the pipeline
+    """
+    model = args.method
+    # get weights
+    weights = read_weights(model)
+    # get hyperparameters (degree needed for polynomial expansion)
+    hyperparameters = read_best_hyperparameters(model)
+    # build folds needs hyperparameter values as individual values and not as lists
+    hyperparameters = {key: hyperparameters[key][0] for key in hyperparameters.keys()}
+    # get test data
+    test_y, test_x, ids_test = read_test_set()
+    # build polynomial expansion and impute/standardize
+    test_x_processed, _, _, _ = build_folds(test_y, test_x, [], -1, hyperparameters, cross_validate=False)
+    # do prediction
+    y_pred = predict_labels(weights, test_x_processed)
+    # save the prediction
+    create_csv_submission(ids_test, y_pred, "test_submission.csv")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="CLI for the run.py which generates the predictions.")
+    parser.add_argument('-m', '--method', type=str, help='Which method to use to predict.', required=True)
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    # parse_args()
-    run()
+    args = parse_args()
+    main(args)
